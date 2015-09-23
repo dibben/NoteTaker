89255ae5c86c0f2b6f8873ee0f98bf12c9f57b54
David Dibben <dibben@ieee.org>
2015-09-23 20:01:52 +0900

    Added license text in source files

--- MessageInterface.cpp
+++ MessageInterface.cpp
@@ -1,8 +1,16 @@
-/*!
-	\file  MessageInterface.cpp
-	\brief The MessageInterface implementation
-	\author David Dibben
-	\version \$Id: MessageInterface.cpp $
+/*
+	NoteTaker is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	NoteTaker is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with NoteTaker.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "MessageInterface.h"
--- NoteSyntaxHighlighter.cpp
+++ NoteSyntaxHighlighter.cpp
@@ -1,9 +1,16 @@
-/*!
-	\file  NoteSyntaxHighlighter.cpp
-	\brief The NoteSyntaxHighlighter implementation
-	\author Author
-	\date \$Date: Mon Sep 21 2015 $
-	\version \$Id: TSearchHighlighter.cpp $
+/*
+	NoteTaker is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	NoteTaker is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with NoteTaker.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "NoteSyntaxHighlighter.h"
